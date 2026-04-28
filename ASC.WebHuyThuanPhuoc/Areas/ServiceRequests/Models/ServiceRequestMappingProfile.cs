fbc249bed9fe689b1042fa49c60ab2de56991905
HuynhHoangHuy <2324802010236@student.tdmu.edu.vn>
2026-04-28 11:25:01 +0700

    xong phan III

---
+++ ASC.WebHuyThuanPhuoc/Areas/ServiceRequests/Models/ServiceRequestMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using ASC.Model.Models;
+using AutoMapper;
+
+namespace ASC.WebHuyThuanPhuoc.Areas.ServiceRequests.Models
+{
+    public class ServiceRequestMappingProfile : Profile
+    {
+        public ServiceRequestMappingProfile()
+        {
+            CreateMap<ServiceRequest, NewServiceRequestViewModel>();
+            CreateMap<NewServiceRequestViewModel, ServiceRequest>();
+        }
+    }
+}
--- ASC.WebHuyThuanPhuoc/Services/DependencyInjection.cs
+++ ASC.WebHuyThuanPhuoc/Services/DependencyInjection.cs
@@ -1,92 +1,93 @@
-using ASC.Business;
-using ASC.Business.Interfaces;
-using ASC.DataAccess;
-using ASC.WebHuyThuanPhuoc.Configuration;
-using ASC.WebHuyThuanPhuoc.Data;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+    using ASC.Business;
+    using ASC.Business.Interfaces;
+    using ASC.DataAccess;
+    using ASC.WebHuyThuanPhuoc.Configuration;
+    using ASC.WebHuyThuanPhuoc.Data;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
 
-namespace ASC.WebHuyThuanPhuoc.Services
-{
-    public static class DependencyInjection
+    namespace ASC.WebHuyThuanPhuoc.Services
     {
-        public static IServiceCollection AddConfig(this IServiceCollection services, IConfiguration config)
+        public static class DependencyInjection
         {
-            var connectionString = config.GetConnectionString("DefaultConnection")
-                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-
-            services.AddOptions();
-            services.Configure<ApplicationSettings>(config.GetSection("AppSettings"));
-
-            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            public static IServiceCollection AddConfig(this IServiceCollection services, IConfiguration config)
             {
-                options.User.RequireUniqueEmail = true;
-            })
-            .AddEntityFrameworkStores<ApplicationDbContext>()
-            .AddDefaultTokenProviders();
+                var connectionString = config.GetConnectionString("DefaultConnection")
+                    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-            services.ConfigureApplicationCookie(options =>
-            {
-                options.LoginPath = "/Identity/Account/Login";
-                options.LogoutPath = "/Identity/Account/Logout";
-                options.AccessDeniedPath = "/Identity/Account/Login";
-            });
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(connectionString));
 
-            var googleIdentitySection = config.GetSection("Authentication:Google:Identity");
-            var googleClientId = googleIdentitySection["ClientId"];
-            var googleClientSecret = googleIdentitySection["ClientSecret"];
+                services.AddOptions();
+                services.Configure<ApplicationSettings>(config.GetSection("AppSettings"));
 
-            if (!string.IsNullOrWhiteSpace(googleClientId) &&
-                !string.IsNullOrWhiteSpace(googleClientSecret))
-            {
-                services
-                    .AddAuthentication()
-                    .AddGoogle(options =>
-                    {
-                        options.ClientId = googleClientId;
-                        options.ClientSecret = googleClientSecret;
-                        options.CallbackPath = "/signin-google";
-                    });
+                services.AddIdentity<IdentityUser, IdentityRole>(options =>
+                {
+                    options.User.RequireUniqueEmail = true;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+                services.ConfigureApplicationCookie(options =>
+                {
+                    options.LoginPath = "/Identity/Account/Login";
+                    options.LogoutPath = "/Identity/Account/Logout";
+                    options.AccessDeniedPath = "/Identity/Account/Login";
+                });
+
+                var googleIdentitySection = config.GetSection("Authentication:Google:Identity");
+                var googleClientId = googleIdentitySection["ClientId"];
+                var googleClientSecret = googleIdentitySection["ClientSecret"];
+
+                if (!string.IsNullOrWhiteSpace(googleClientId) &&
+                    !string.IsNullOrWhiteSpace(googleClientSecret))
+                {
+                    services
+                        .AddAuthentication()
+                        .AddGoogle(options =>
+                        {
+                            options.ClientId = googleClientId;
+                            options.ClientSecret = googleClientSecret;
+                            options.CallbackPath = "/signin-google";
+                        });
+                }
+
+                services.AddStackExchangeRedisCache(options =>
+                {
+                    options.Configuration = config.GetSection("CacheSettings:CacheConnectionString").Value;
+                    options.InstanceName = config.GetSection("CacheSettings:CacheInstance").Value;
+                });
+
+                return services;
             }
 
-            services.AddStackExchangeRedisCache(options =>
+            public static IServiceCollection AddMyDependencyGroup(this IServiceCollection services)
             {
-                options.Configuration = config.GetSection("CacheSettings:CacheConnectionString").Value;
-                options.InstanceName = config.GetSection("CacheSettings:CacheInstance").Value;
-            });
+                services.AddScoped<IMasterDataOperations, MasterDataOperations>();
+                services.AddScoped<IMasterDataCacheOperations, MasterDataCacheOperations>();
+                services.AddScoped<IServiceRequestOperations, ServiceRequestOperations>();
 
-            return services;
-        }
+                services.AddControllersWithViews();
+                services.AddRazorPages();
 
-        public static IServiceCollection AddMyDependencyGroup(this IServiceCollection services)
-        {
-            services.AddScoped<IMasterDataCacheOperations, MasterDataCacheOperations>();
-            services.AddControllersWithViews();
-            services.AddRazorPages();
+                services.AddTransient<IEmailSender, AuthMessageSender>();
+                services.AddTransient<ISmsSender, AuthMessageSender>();
 
-            services.AddTransient<IEmailSender, AuthMessageSender>();
-            services.AddTransient<ISmsSender, AuthMessageSender>();
+                services.AddSingleton<IIdentitySeed, IdentitySeed>();
+                services.AddScoped<DbContext, ApplicationDbContext>();
+                services.AddScoped<UnitOfWork, UnitOfWork>();
+                services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-            services.AddSingleton<IIdentitySeed, IdentitySeed>();
-            services.AddScoped<DbContext, ApplicationDbContext>();
-            services.AddScoped<UnitOfWork, UnitOfWork>();
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
+                services.AddMemoryCache();
+                services.AddScoped<INavigationCacheOperations, NavigationCacheOperations>();
 
-            services.AddMemoryCache();
-            services.AddScoped<INavigationCacheOperations, NavigationCacheOperations>();
+                services.AddAutoMapper(typeof(ApplicationDbContext));
+  
+                services.AddSession();
+                services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-            services.AddScoped<IMasterDataOperations, MasterDataOperations>();
-            services.AddAutoMapper(typeof(ApplicationDbContext));
-
-            services.AddDistributedMemoryCache();
-            services.AddSession();
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-            return services;
-        }
+                return services;
+            }
     }
-}
+    }
